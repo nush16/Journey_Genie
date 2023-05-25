01fdb5a48ec873366ab1d6c7780f22480c5d5477
Anushke <115445200+nush16@users.noreply.github.com>
2023-05-25 16:51:45 +1000

    added state for rating type

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
 import useStyles from './Liststyles.js';
 
 
 const List = () => {
     const classes = useStyles();
+    // State for the selected type
+    const [type, setType] = useState('');
+    // State for the selected type
+    const[rating, setRating] = useState('');
     
     return (
         <div className={classes.container}>
@@ -14,7 +18,7 @@ const List = () => {
                 <FormControl className={classes.formControl}>
                     <InputLabel id="type">Type</InputLabel>
                     {/* Selecting the type */}
-                    <Select value={type} onChange={''}>
+                    <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
                         <MenuItem value="restaurants">Restaurants</MenuItem>
                         <MenuItem value="hotels">Hotels</MenuItem>
                         <MenuItem value="attractions">Attractions</MenuItem>
@@ -25,7 +29,7 @@ const List = () => {
                 <FormControl className={classes.formControl}>
                     <InputLabel id="rating">Rating</InputLabel>
                     {/* Selecting the rating */}
-                    <Select value={type} onChange={''}>
+                    <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
                         <MenuItem value="">All</MenuItem>
                         <MenuItem value="3">Above 3.0</MenuItem>
                         <MenuItem value="4">Above 4.0</MenuItem>
