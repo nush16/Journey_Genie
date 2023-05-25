2ac985479b69ca673c22e6c4794538dd0dee4b8d
Anushke <115445200+nush16@users.noreply.github.com>
2023-05-25 17:45:23 +1000

    added hook to  hook to fetch data from the getPlacesData function and update the state with the fetched data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import { CssBaseline, Grid} from '@material-ui/core';
-
-
+import { getPlacesData} from './api/ApiCall'; 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 
 const App = () => {
-    
+    const [places, setPlaces] = useState([]); // State for the Places
+    const [coordinates, setCoordinates] = useState({}); // State for the Coordinates
+    const [bounds, setBounds] = useState({}); // State for the Bounds
+
+   // Get location data from API 
+    useEffect(() => {
+        // Fetch data using the getPlacesData function
+        getPlacesData()
+            .then((data) => {
+                // Update the state with the fetched data
+                setPlaces(data);
+            })
+      }, []);
+
     return (
         <>
             <CssBaseline />
