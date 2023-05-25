eac46fa3d977ad885fffd223cc7819866afcf8cf
Anushke <115445200+nush16@users.noreply.github.com>
2023-05-26 00:05:06 +1000

    search functionality added to search bar. autocomplete to fill out destinations

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,24 @@
-import React from "react";
-// import { Autocomplete } from '@react-google-maps/api';
+import React,{useState} from "react";
+import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import useStyles from './Headerstyles';
 
-const Header = () => {
+const Header = ({setCoordinates}) => {
     const classes = useStyles();
+    const [autocomplete, setAutocomplete] = useState(null);
+
+    // function called 'onLoad' that takes a parameter 'fillC' and sets the 'autocomplete' state with that value
+    const onLoad = (fillC) => setAutocomplete(fillC);
+
+    //event handler for when the selected place in the autocomplete component changes
+    const onPlaceChanged = () => {
+        const lat = autocomplete.getPlace().geometry.location.lat(); // Retrieves the latitude from the selected place using the autocomplete component.
+        const lng = autocomplete.getPlace().geometry.location.lng(); // Retrieves the longitude from the selected place using the autocomplete component.
+      
+        setCoordinates({ lat, lng }); // Sets the 'coordinates' state with the extracted latitude and longitude.
+      }
+    
     return (
         // App bar at the top of the page
         <AppBar position="static">
@@ -21,7 +34,7 @@ const Header = () => {
                         Where do you want to go?
                     </Typography>
                     {/* Allows users to select from a predefined set of options */}
-                    {/* <Autocomplete> */}
+                    <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
                     {/* Represents the search bar container */}
                         <div className={classes.search}>
                             {/* Contains the search icon */}
@@ -31,7 +44,7 @@ const Header = () => {
                             {/* Represents the text input for the search bar */}
                             <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
                         </div>
-                    {/* </Autocomplete> */}
+                    </Autocomplete>
                 </Box>       
             </Toolbar>
         </AppBar>
