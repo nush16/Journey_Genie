af3c1b2f04b240ed0f73cff43c08e27429d14141
Anushke <115445200+nush16@users.noreply.github.com>
2023-05-25 16:05:52 +1000

    added comments to header section and also fixed up index.js for react18

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { Autocomplete } from '@react-google-maps/api';
+// import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-import useStyles from './styles';
+import useStyles from './Headerstyles';
 
 const Header = () => {
     const classes = useStyles();
     return (
+        // App bar at the top of the page
         <AppBar position="static">
+            {/* Contains the content of the app bar */}
             <Toolbar className={classes.toolbar}>
+            {/* Display the logo of the app */}
                 <Typography variant="h5" className={classes.title}>
-                    Travel Advisor
+                    Logo
                 </Typography>
                 <Box display="flex">
+                    {/* display Subtitle with additional information */}
                     <Typography variant ="h6" className={classes.title}>
-                        Explore new places
+                        Whats around me?
                     </Typography>
+                    {/* Allows users to select from a predefined set of options */}
                     {/* <Autocomplete> */}
+                    {/* Represents the search bar container */}
                         <div className={classes.search}>
+                            {/* Contains the search icon */}
                             <div className={classes.searchIcon}>
                                 <SearchIcon/>
                             </div>
+                            {/* Represents the text input for the search bar */}
                             <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
                         </div>
                     {/* </Autocomplete> */}
