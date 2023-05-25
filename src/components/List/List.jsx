40073bf09f50af8c6c1d8be76be379709f653e44
Anushke <115445200+nush16@users.noreply.github.com>
2023-05-25 16:43:17 +1000

    basic list items added for type and rating

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+import useStyles from './Liststyles.js';
+
 
 const List = () => {
+    const classes = useStyles();
+    
     return (
-        <h1>List</h1>
-    );
-}
+        <div className={classes.container}>
+            {/* Heading */}
+            <Typography variant="h4">Whats Around Me?</Typography>
+                 {/* Type filter */}
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="type">Type</InputLabel>
+                    {/* Selecting the type */}
+                    <Select value={type} onChange={''}>
+                        <MenuItem value="restaurants">Restaurants</MenuItem>
+                        <MenuItem value="hotels">Hotels</MenuItem>
+                        <MenuItem value="attractions">Attractions</MenuItem>
+                    </Select>
+                </FormControl>
+
+                {/* Rating filter */}
+                <FormControl className={classes.formControl}>
+                    <InputLabel id="rating">Rating</InputLabel>
+                    {/* Selecting the rating */}
+                    <Select value={type} onChange={''}>
+                        <MenuItem value="">All</MenuItem>
+                        <MenuItem value="3">Above 3.0</MenuItem>
+                        <MenuItem value="4">Above 4.0</MenuItem>
+                        <MenuItem value="4.5">Above 4.5</MenuItem>
+                    </Select>
+                </FormControl>
+    </div>
+  );
+};
 
 export default List;
