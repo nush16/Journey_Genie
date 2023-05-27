820ea7f86d7149622ceee972d3bd79c6cc514824
Anushke <115445200+nush16@users.noreply.github.com>
2023-05-27 12:51:27 +1000

    fiexed issue with MapStyles.js

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import useStyles from "./Mapstyles.js";
+import useStyles from "./MapStyles.js";
 import MapDesign from "./MapDesign.js";
 
 const Map = ({
