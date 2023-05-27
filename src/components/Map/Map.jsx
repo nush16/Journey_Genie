0d32647b6cd0cfdf7bacc7ea50262e5d3aa96280
Anushke <115445200+nush16@users.noreply.github.com>
2023-05-27 12:04:24 +1000

    fixed error with map styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import Rating from "@material-ui/lab/Rating";
-// import Design from './mapDesign.js';
 import useStyles from "./Mapstyles.js";
 import MapDesign from "./MapDesign.js";
 
